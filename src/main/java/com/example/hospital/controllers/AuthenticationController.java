6d8516ac9c9c14bd99bb081d216d332e5b56208e
igorperonico <igormtpr@gmail.com>
2024-08-30 00:30:17 -0300

    create AuthenticationController

---
+++ src/main/java/com/example/hospital/controllers/AuthenticationController.java
@@ -0,0 +1,57 @@
+package com.example.hospital.controllers;
+
+import com.example.hospital.domain.User;
+import com.example.hospital.domain.dtos.AuthDTO;
+import com.example.hospital.domain.dtos.LoginResponseDTO;
+import com.example.hospital.domain.dtos.RegisterDTO;
+import com.example.hospital.infra.security.TokenService;
+import com.example.hospital.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity logout(@RequestBody String token) {
+        tokenService.revokeToken(token);
+        return ResponseEntity.ok("Logged out successfully");
+    }
+}
